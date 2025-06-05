650fcc829fbbdbbe712a6873d50ce529ddc40455
boobacar <boubsfal@gmail.com>
2025-06-05 00:15:25 +0000

    fixing competences grid

--- src/components/CompetencesGrid.jsx
+++ src/components/CompetencesGrid.jsx
@@ -30,7 +30,7 @@ const CompetencesGrid = () => {
       </motion.h2>
 
       <motion.div
-        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6"
+        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6"
         variants={container}
         initial="hidden"
         whileInView="show"
