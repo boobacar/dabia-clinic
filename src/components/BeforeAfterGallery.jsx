47c68d3a60ebad2dcbde376920942eefb6a59920
boobacar <boubsfal@gmail.com>
2025-06-08 21:13:17 +0000

    still fixing slider

--- src/components/BeforeAfterGallery.jsx
+++ src/components/BeforeAfterGallery.jsx
@@ -13,7 +13,7 @@ export default function BeforeAfterGallery() {
       </h2>
 
       <div
-        className="before-after-wrapper grid gap-6
+        className=" grid gap-6
                       sm:grid-cols-1
                       md:grid-cols-2
                       lg:grid-cols-3"
